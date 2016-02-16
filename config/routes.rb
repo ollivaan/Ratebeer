452b486733faf61aaed727ca0671892c9d760370
Ollipekka Väänänen <ollivaan@dhcp-asv-101.helsinki.fi>
2016-02-16 14:08:24 +0200

    1 tehtävä

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,9 @@ Rails.application.routes.draw do
 
   root 'breweries#index'
 
+  get 'places', to: 'places#index'
+  post 'places', to:'places#search'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
