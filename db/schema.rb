1cffae087fe119d019dfd11563b657f7016910d0
Ollipekka Väänänen <ollivaan@dhcp-asv-101.helsinki.fi>
2016-02-21 18:34:18 +0200

    vko5

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160129174824) do
+ActiveRecord::Schema.define(version: 20160221162546) do
 
   create_table "beer_clubs", force: :cascade do |t|
     t.string   "name"
@@ -49,6 +49,12 @@ ActiveRecord::Schema.define(version: 20160129174824) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+  end
+
+  create_table "styles", force: :cascade do |t|
+    t.string   "style"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
